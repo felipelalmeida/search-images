4714833a33324b5c0aab8bec02f0472681fedba6
Felipe Almeida <almeida.felipe98@gmail.com>
2021-05-31 21:00:42 -0300

    adição useEffect

--- src/App.js
+++ src/App.js
@@ -6,32 +6,41 @@ function App() {
 
   const [images, setImages] = useState([])
   const [page, setPage] = useState(1)
+  const [pesquisa, setPesquisa] = useState('')
+  const [totalPages, setTotalPages] = useState(0)
 
+  useEffect(() => {
+    const consultaAPI = async () => {
+      if (pesquisa === '') return
 
-  async function searchSubmit(input) {
-    const response = await api.get(`?key=${key}&q=${input}
-    &lang=pt&per_page=20&page=${page}`)
-    console.log(response.data)
-    setImages(response.data.hits)
-  }
+      const response = await api.get(`?key=${key}&q=${pesquisa}
+      &lang=pt&per_page=20&page=${page}`)
 
+      setImages(response.data.hits)
+      console.log(response.data)
+      setTotalPages(Math.ceil(response.data.totalHits / 20))
+    }
+    consultaAPI()
+
+    window.location.href = '#ancora'
+
+  }, [pesquisa, page])
 
   function previousPage() {
-    if (page === 1) {
-      return
-    }
+    if (page === 1) return
     setPage(page - 1)
   }
 
   function nextPage() {
+    if (page >= totalPages) return
     setPage(page + 1)
   }
 
 
   return (
-    <div>
-      <SearchBar onSubmit={searchSubmit} />
-      <section className="cards">
+    <div href="#" id="ancora">
+      <SearchBar setPesquisa={setPesquisa} setPage={setPage} />
+      <section className="cards" >
         {images.map((image) => {
           return (
             <div key={image.id}>
@@ -44,10 +53,16 @@ function App() {
             </div>
           )
         })}
-        <button onClick={previousPage}>Anterior</button>
-        <button onClick={nextPage}>Próxima</button>
       </section>
-    </div>
+      { (totalPages !== 0) && 
+        <div className="pagination">
+          <button onClick={previousPage}>Anterior</button>
+          <p> {page} </p>
+          <button onClick={nextPage}>Próxima</button>
+        </div>
+      
+      }
+    </div >
   );
 }
 
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,50 +1,22 @@
-// import { useState } from 'react'
-// import './styles.css'
-
-//  const SearchBar = ({ setSearch }) => {
-
-
-//     const [input, setInput] = useState('')
-
-//     const formSubmit = (e) => {
-//         e.preventDefault()
-//         console.log('form submit: ' + input)
-//         setSearch(input)
-//     }
-
-//     return (
-//         <div className="container">
-//             <form onSubmit={formSubmit} className="search-bar">
-//                 <input type="text" placeholder="Digite sua pesquisa"
-//                     value={input} onChange={(e) => setInput(e.target.value)} />
-//                 <button type="submit">Pesquisar</button>
-//             </form>
-//         </div>
-//     )
-// }
-
-// export default SearchBar
-
-
-
-
-import { useState } from 'react'
+import { useState } from "react"
 import './styles.css'
 
-export default function SearchBar(props) {
-
-
+const SearchBar = ({ setPesquisa, setPage }) => {
     const [input, setInput] = useState('')
 
-    function formSubmit(e) {
+    const buscarImagens = (e) => {
         e.preventDefault()
-        console.log('form submit: ' + input)
-        props.onSubmit(input)
+        if (input === '') {
+            alert('Pesquisa vazia')
+            return
+        }
+        setPesquisa(input)
+        setPage(1)
     }
 
     return (
         <div className="container">
-            <form onSubmit={(e) => formSubmit(e)} className="search-bar">
+            <form onSubmit={buscarImagens} className="search-bar">
                 <input type="text" placeholder="Digite sua pesquisa"
                     value={input} onChange={(e) => setInput(e.target.value)} />
                 <button type="submit">Pesquisar</button>
@@ -52,3 +24,5 @@ export default function SearchBar(props) {
         </div>
     )
 }
+
+export default SearchBar
