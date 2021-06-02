e051866c9ab9bd6953c3586cf39638cfc37d899e
Felipe Almeida <almeida.felipe98@gmail.com>
2021-06-01 21:51:51 -0300

    mensagem caso nao ache imagens

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { NotFound } from './components/NotFound'
 import SearchBar from './components/SearchBar'
 import api, { key } from './services/api'
 
@@ -8,6 +9,7 @@ function App() {
   const [page, setPage] = useState(1)
   const [pesquisa, setPesquisa] = useState('')
   const [totalPages, setTotalPages] = useState(0)
+  const [noResult, setNoResult] = useState(false)
 
   useEffect(() => {
     const consultaAPI = async () => {
@@ -16,30 +18,32 @@ function App() {
       const response = await api.get(`?key=${key}&q=${pesquisa}
       &lang=pt&per_page=20&page=${page}`)
 
+
+      response.data.totalHits === 0 ? setNoResult(true) : setNoResult(false)
+
       setImages(response.data.hits)
       console.log(response.data)
       setTotalPages(Math.ceil(response.data.totalHits / 20))
     }
     consultaAPI()
 
-    window.location.href = '#ancora'
+    window.location.href = '#'
 
   }, [pesquisa, page])
 
   function previousPage() {
-    if (page === 1) return
     setPage(page - 1)
   }
 
   function nextPage() {
-    if (page >= totalPages) return
     setPage(page + 1)
   }
 
 
   return (
-    <div href="#" id="ancora">
+    <div href="#" >
       <SearchBar setPesquisa={setPesquisa} setPage={setPage} />
+      {(noResult) && <NotFound />}
       <section className="cards" >
         {images.map((image) => {
           return (
---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,7 @@
+import './styles.css'
+
+export function NotFound() {
+    return (
+        <h2>Nenhum resultado encontrado...</h2>
+    )
+}
